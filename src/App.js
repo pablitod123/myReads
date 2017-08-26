0518e198573ade859b9774ef100a82e7a6720b9a
Paul Styslinger <paulstyslinger@Pauls-MacBook-Air.local>
2017-08-26 16:12:32 -0500

    search now works

--- src/App.js
+++ src/App.js
@@ -6,35 +6,46 @@ import { Link } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import './App.css'
 
-class BooksApp extends React.Component {
 
+class BooksApp extends React.Component {
+  state = {
+    books: [],
+    results: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+      // console.log(JSON.stringify(books))
+    })
+  }
+
+  searchBooks(query, maxResults) {
+      BooksAPI.search(query, maxResults).then((results) => {
+        this.setState({ results })
+        // console.log(this.state.results)
+      })
+    }
+
+  addBook(book, shelf) {
+    BooksAPI.update(book, shelf)
+  }
 
   render() {
     return (
       <div className="app">
-      <Route exact path="/search" render={( {history} ) => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-                
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+      <Route exact path="/search" render={() => (
+          <SearchBooks results={this.state.results} onSearch={(query) => {
+            this.searchBooks(query, 20)
+          }}/>
         )}/>
 
-      <Route exact path="/" render={() => (
+      <Route exact path="/" render={( {history} ) => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            <Bookshelf />
-            <Bookshelf />
-            <Bookshelf />
+            <Bookshelf books={this.state.books}/>
             <div className="open-search">
               <Link to="/search">Add a book</Link>
             </div>
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import serializeForm from 'form-serialize'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends Component {
+	handleSubmit = (e) => {
+		e.preventDefault()
+		const values = serializeForm(e.target, {hash: true})
+		this.props.onSearch(values.query, 20)
+		console.log(this.props.results)
+	}
+
+
 	render() {
-		return (
-			<div className='search-books'>
-
-
-			</div>
+		return (	
+			<div className="search-books">
+	            <div className="search-books-bar">
+	              <Link className="close-search" to="/">Close</Link>
+	              <div className="search-books-input-wrapper">
+	              	<form onSubmit={this.handleSubmit}>
+	                	<input type="text" name="query" placeholder="Search by title or author"/>
+	                </form>
+	              </div>
+	            </div>
+	            <div className="search-books-results">
+	              <ol className="books-grid">
+	              	{this.props.results.map((result, i) => (
+								<div className="book" key={i}>
+			                         <div className="book-top">
+			                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${result.imageLinks.thumbnail})` }}></div>
+				                            <div className="book-shelf-changer">
+				                              <select>
+				                                <option value="none" disabled>Move to...</option>
+				                                <option value="currentlyReading">Currently Reading</option>
+				                                <option value="wantToRead">Want to Read</option>
+				                                <option value="read">Read</option>
+				                                <option value="none">None</option>
+				                              </select>
+				                            </div>
+				                          </div>
+			                          <div className="book-title">{result.title}</div>
+			                        <div className="book-authors">{result.subtitle}</div>
+			                     </div>
+							))}
+	              </ol>
+	            </div>
+	         </div>
 		)
 	}
 }
